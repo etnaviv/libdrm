eeaa921f53179ccb5badf319e4efcb39b30dfebd
Christian Gmeiner <christian.gmeiner@gmail.com>
2014-06-07 00:34:09 +0200

    etnaviv: add very very basic ringbuffer impl
    etna_ringbuffer_flush() is a stub!
    
    Signed-off-by: Christian Gmeiner <christian.gmeiner@gmail.com>

---
+++ etnaviv/etnaviv_ringbuffer.c
@@ -0,0 +1,87 @@
+/*
+ * Copyright (C) 2014 Etnaviv Project
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ * Authors:
+ *    Christian Gmeiner <christian.gmeiner@gmail.com>
+ */
+
+#include <assert.h>
+
+#include "etnaviv_drmif.h"
+#include "etnaviv_priv.h"
+#include "etnaviv_ringbuffer.h"
+
+struct etna_ringbuffer * etna_ringbuffer_new(struct etna_pipe *pipe,
+		uint32_t size)
+{
+	struct etna_ringbuffer *ring;
+
+	ring = calloc(1, sizeof(*ring));
+	if (!ring) {
+		ERROR_MSG("allocation failed");
+		goto fail;
+	}
+
+	ring->ring_bo = etna_bo_new(pipe->dev, size, 0);
+	if (!ring->ring_bo) {
+		ERROR_MSG("ringbuffer allocation failed");
+		goto fail;
+	}
+
+	ring->size = size;
+	ring->pipe = pipe;
+	ring->start = etna_bo_map(ring->ring_bo);
+	ring->end = &(ring->start[size/4]);
+
+	ring->cur = ring->last_start = ring->start;
+
+	return ring;
+
+fail:
+	if (ring)
+		etna_ringbuffer_del(ring);
+	return NULL;
+}
+
+void etna_ringbuffer_del(struct etna_ringbuffer *ring)
+{
+	if (ring->ring_bo)
+		etna_bo_del(ring->ring_bo);
+	free(ring);
+}
+
+void etna_ringbuffer_reset(struct etna_ringbuffer *ring)
+{
+	uint32_t *start = ring->start;
+	ring->cur = ring->last_start = start;
+}
+
+int etna_ringbuffer_flush(struct etna_ringbuffer *ring)
+{
+	/* TODO */
+	return 0;
+}
+
+uint32_t etna_ringbuffer_timestamp(struct etna_ringbuffer *ring)
+{
+	return ring->last_timestamp;
+}
---
+++ etnaviv/etnaviv_ringbuffer.h
@@ -0,0 +1,47 @@
+/*
+ * Copyright (C) 2014 Etnaviv Project
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ * Authors:
+ *    Christian Gmeiner <christian.gmeiner@gmail.com>
+ */
+
+#ifndef ETNAVIV_RINGBUFFER_H_
+#define ETNAVIV_RINGBUFFER_H_
+
+struct etna_ringbuffer {
+	int size;
+	uint32_t *cur, *end, *start, *last_start;
+	struct etna_pipe *pipe;
+	uint32_t last_timestamp;
+
+	struct etna_bo *ring_bo;
+};
+
+struct etna_ringbuffer * etna_ringbuffer_new(struct etna_pipe *pipe,
+		uint32_t size);
+void etna_ringbuffer_del(struct etna_ringbuffer *ring);
+void etna_ringbuffer_reset(struct etna_ringbuffer *ring);
+
+int etna_ringbuffer_flush(struct etna_ringbuffer *ring);
+uint32_t etna_ringbuffer_timestamp(struct etna_ringbuffer *ring);
+
+#endif /* ETNAVIV_RINGBUFFER_H_ */
